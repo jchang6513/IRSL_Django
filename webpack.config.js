842313b92624ef08d7758ee9d288cb8baab05a24
Yi-Wei <jchang6513@gmail.com>
2018-09-06 09:31:52 +0800

    fix href issue

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
-    entry: SRC_DIR + "/app/link.jsx",
+    entry: SRC_DIR + "/app/course.jsx",
     output: {
         path: DIST_DIR + "/app",
-        filename: "link.js",
+        filename: "course.js",
         publicPath: "/app/"
     },
     module: {
