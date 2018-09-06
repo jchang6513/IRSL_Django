b82c10679a7ae72bc9a3a4ee8feaff892ea7850e
Yi-Wei Chang <jchang6513@gmail.com>
2018-09-06 11:25:04 +0800

    navbar change color on scroll

--- static/js/style.js
+++ static/js/style.js
@@ -10,36 +10,20 @@ $(function() {
     },1000)
 });
 
-//$(document).on('scroll', function (e) {
-//    $('.navbar').css('opacity', ( (1-$(document).scrollTop()/500)>0.8 ? (1-$(document).scrollTop()/500) : 0.8));
-//});
-
-// $(document).on('scroll', function (e) {
-//     if ($(window).width() > 991) {
-//         $('.navbar-brand img').css('height', ((40-$(document).scrollTop()/40)>30 ? (40-$(document).scrollTop()/40) : 30));
-//     }
-// });
-//
-// $(document).on('scroll', function (e) {
-//     if ($(window).width() > 991) {
-//         $('#navbarNav').css('font-size', ((17-$(document).scrollTop()/200)>15 ? (17-$(document).scrollTop()/200) : 15));
-//     }
-// });
-
 $(document).on('scroll', function (e) {
     if ($(document).scrollTop() > $('.jumbotron').height()) {
       console.log('1,black')
-      $(".navbar").removeClass("navbar-faded");
+      $(".navbar").removeClass("navbar-light");
       $(".navbar").removeClass("bg-faded");
       $(".navbar").addClass("navbar-inverse");
       $(".navbar").addClass("bg-inverse");
       $(".navbar-logo").attr("src","/static/images/irsl_h_w.png")
-      $(".hamburger").removeClass("hamburger-light");      
+      $(".hamburger").removeClass("hamburger-light");
     } else {
       console.log('white')
       $(".navbar").removeClass("navbar-inverse");
       $(".navbar").removeClass("bg-inverse");
-      $(".navbar").addClass("navbar-faded");
+      $(".navbar").addClass("navbar-light");
       $(".navbar").addClass("bg-faded");
       $(".navbar-logo").attr("src","/static/images/irsl_h_b.png")
       $(".hamburger").addClass("hamburger-light");
